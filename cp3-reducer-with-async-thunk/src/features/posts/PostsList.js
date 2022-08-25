59239898f6051e15ceae7da9d9c872207d60d442
Sizar Corpse <sizarcorpse@gmail.com>
2022-08-25 23:21:54 +0600

    reducer with redux async thunk

---
+++ cp3-reducer-with-async-thunk/src/features/posts/PostsList.js
@@ -0,0 +1,47 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import PostsExcerpt from "./PostsExcerpt";
+import {
+  fetchPosts,
+  getPostsError,
+  getPostsStatus,
+  selectAllPosts,
+} from "./postsSlice";
+
+const PostsList = () => {
+  const dispatch = useDispatch();
+
+  const posts = useSelector(selectAllPosts);
+  const postStatus = useSelector(getPostsStatus);
+  const error = useSelector(getPostsError);
+
+  useEffect(() => {
+    if (postStatus === "idle") {
+      dispatch(fetchPosts());
+    }
+  }, [postStatus, dispatch]);
+
+  // order by post date descending
+  let content;
+  if (postStatus === "loading") {
+    content = <p>"Loading..."</p>;
+  } else if (postStatus === "succeeded") {
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date));
+    content = orderedPosts.map((post) => (
+      <PostsExcerpt key={post.id} post={post} />
+    ));
+  } else if (postStatus === "failed") {
+    content = <p>{error}</p>;
+  }
+
+  return (
+    <section>
+      PostsList
+      {content}
+    </section>
+  );
+};
+
+export default PostsList;
---
+++ cp3-reducer-with-async-thunk/src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import { store } from "./app/store";
+import "./index.css";
+
+import { fetchUsers } from "./features/users/usersSlice";
+
+store.dispatch(fetchUsers());
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
